a1a5d95979261c998ab103d47f706042b7903395
kk <karthik.kulkarni@vodafone.com>
2018-07-26 16:21:31 +1200

    quotes CRUD

--- boilerplate/App.js
+++ boilerplate/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 
 import store from "./app/store"; //Import the store
-import Home from "./app/components/home"; //Import the component file
+import Main from "./app/index";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Home />
+        <Main />
       </Provider>
     );
   }
